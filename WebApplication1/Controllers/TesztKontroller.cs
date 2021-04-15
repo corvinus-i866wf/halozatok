892b1cc446ab50b9372e76986be081206bae6fb8
Gergely Masek <gergely.masek@stud.uni-corvinus.hu>
2021-04-15 11:03:26 +0200

    gyak8

---
+++ WebApplication1/Controllers/TesztKontroller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztKontroller : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosido = DateTime.Now.ToShortTimeString();
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                Content = pontosido
+            };
+        }
+     
+
+    }
+
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -16,6 +16,7 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,7 +30,7 @@ namespace WebApplication1
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseRouting();
-            
+
             /*app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
@@ -37,6 +38,10 @@ namespace WebApplication1
                     //await context.Response.WriteAsync("Hello World!");
                 });
             });*/
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
