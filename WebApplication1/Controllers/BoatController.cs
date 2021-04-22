0fc6fb511250b5c49b85bb0ea600d17a1afe9ad2
Gergely Masek <gergely.masek@stud.uni-corvinus.hu>
2021-04-22 14:41:24 +0200

    mukodo leptetheto kerdesbank képek valamiért nem müködnek válaszok igen

--- WebApplication1/Controllers/BoatController.cs
+++ WebApplication1/Controllers/BoatController.cs
@@ -18,8 +18,26 @@ namespace WebApplication1.Controllers
         {
             HajostesztContext context = new HajostesztContext();
             var kérdések = from x in context.Questions select x.QuestionText;
-            return new JsonResult(kérdések);
+           
 
+            if (kérdések == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+            return new JsonResult(kérdések);
+        }
+    }
+    public class BoatController1 : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
         }
     }
 }
--- WebApplication1/wwwroot/hetedik.js
+++ WebApplication1/wwwroot/hetedik.js
@@ -1,13 +1,34 @@
 ﻿var kerdes;
 var helyzet = 0;
+var jovalasz;
 
 window.onload =letöltés()
 
 function letöltés() {
-    fetch('/questions.json')
+    fetch('/questions/1')
         .then(response => response.json())
-        .then(data => letöltésBefejeződött(data)
-        )
+        .then(data => kérdésMegjelenítés(data)
+        );
+}
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}    
+function válaszfeldolgozás(válasz) {
+    if (!válasz.ok) {
+        console.error(`Hibás válasz: ${response.status}`)
+    }
+    else {
+        return válasz.json()
+    }
 }
 function letöltésBefejeződött(d) {
     console.log("Sikeres letöltés")
@@ -15,7 +36,7 @@ function letöltésBefejeződött(d) {
     kerdes = d;
     kérdésMegjelen(helyzet);
 }
-function kérdésMegjelen(k) {
+/*function kérdésMegjelen(k) {
     let ide = document.getElementById("kerdes");
     ide.innerHTML = kerdes[k].questionText
 
@@ -28,11 +49,19 @@ function kérdésMegjelen(k) {
     if (document.getElementById("keptag").src != "") {
         document.getElementById("keptag").src = kerdes[k].image
     }
-   
+}*/
+function kérdésMegjelenítés(kerdes) {
+    console.log(kerdes);
+    document.getElementById("kerdes").innerText = kerdes.questionText
+    document.getElementById("valasz1").innerText = kerdes.answer1
+    document.getElementById("valasz2").innerText = kerdes.answer2
+    document.getElementById("valasz3").innerText = kerdes.answer3
+    document.getElementById("kep").src = "https://szoft1.comeback.hu/hajo/" + kerdes.image;
+    jovalasz = kerdes.correctAnswer;
     
 }
 function Léptetelore() {
-    if (helyzet == kerdes.length-1) {
+   /* if (helyzet == kerdes.length-1) {
         helyzet = 0;
         kérdésMegjelen(helyzet);
         clear();
@@ -41,10 +70,21 @@ function Léptetelore() {
         helyzet++;
         kérdésMegjelen(helyzet);
         clear();
+    }*/
+    if (helyzet == 858) {
+        
+        kérdésBetöltés(0);
+        helyzet = 0;
+       
     }
+    else {
+    helyzet++;
+    kérdésBetöltés(helyzet);
+    }
+    clear();
 }
 function Leptethatra() {
-    if (helyzet == 0) {
+   /* if (helyzet == 0) {
         helyzet = kerdes.length - 1;
         kérdésMegjelen(helyzet);
         clear();
@@ -53,18 +93,37 @@ function Leptethatra() {
         helyzet--;
         kérdésMegjelen(helyzet);
         clear();
+    }*/
+    if (helyzet == 0) {
+       
+        kérdésBetöltés(0);
+        helyzet = 858;
+        
     }
+    else {
+        helyzet--;
+        kérdésBetöltés(helyzet);
+    }
+    clear();
+   
 }
 function ellenoriz(k) {
+   /* let jovalasz= kerdes["correctAnswer"]
     let elem = document.getElementById("valasz" + k);
-    if (kerdes[helyzet]["correctAnswer"]==k) {
+    if (jovalasz==k) {
         elem.style.backgroundColor = "green";
     }
     else {
         elem.style.backgroundColor = "red";
-        let jovalasz = document.getElementById("valasz" + kerdes[helyzet]["correctAnswer"]);
-        jovalasz.style.backgroundColor = "green";
-    }       
+        let megold = document.getElementById("valasz" + kerdes["correctAnswer"]);
+        megold.style.backgroundColor = "green";
+    }    */
+    
+    
+    let elem = document.getElementById("valasz" + k);
+    console.log(jovalasz);
+    if (jovalasz == k) { elem.style.backgroundColor = "green" }
+    else { elem.style.backgroundColor="red"}
 }
 function clear() {
     for (var i = 1; i < 4; i++) {
